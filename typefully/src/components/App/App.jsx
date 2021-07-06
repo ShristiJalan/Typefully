50c40b6ed27cf348d4868e4840ed136abd66c77c
ShristiJalan <shristijalan1010@gmail.com>
2021-07-06 16:33:01 +0530

    navbar background

---
+++ typefully/src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./App.css";
+
+class App extends React.Component{
+    render(){
+        return(
+
+        <div className = "app">
+
+            {/* Nav Section */}
+            <h1>This is the nav bar!</h1>
+
+            {/* Landing Page */}
+
+            {/* challenge Section */}
+
+            {/* Footer */}
+
+
+        </div>
+
+        );
+    }
+}
+
+export default App;
